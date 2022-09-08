391fc886964fe51f9baa03156d4d64db96db3a28
abbasinfoicon <uid3@infoiconsoftware.com>
2022-09-08 18:31:58 +0530

    css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -56,15 +56,15 @@
                 "<img src='assets/img/right-icon.png' class='right-icon'>"
             ],
             autoplayHoverPause: true,
-            responsive:{
-                0:{
-                    items:1
+            responsive: {
+                0: {
+                    items: 1
                 },
-                600:{
-                    items:3
+                600: {
+                    items: 3
                 },
-                1000:{
-                    items:5
+                1000: {
+                    items: 5
                 }
             }
         });
@@ -85,5 +85,43 @@
 
 
     });
+
+    // tooltips start
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    // tooltips end
+
+    // tinymce-editor start
+    tinymce.init({
+        selector: "#editor",
+        menubar: false,
+        statusbar: false,
+        plugins:
+            "autoresize anchor autolink charmap code codesample directionality fullpage help hr image imagetools insertdatetime link lists media nonbreaking pagebreak preview print searchreplace table template textpattern toc visualblocks visualchars",
+        toolbar:
+            "bold italic underline forecolor | blocks | link | alignleft aligncenter alignright | bullist numlist | blockquote| image",
+        skin: "bootstrap",
+        toolbar_drawer: "floating",
+        min_height: 200,
+        autoresize_bottom_margin: 16,
+        setup: (editor) => {
+            editor.on("init", () => {
+                editor.getContainer().style.transition =
+                    "border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out";
+            });
+            editor.on("focus", () => {
+                (editor.getContainer().style.boxShadow =
+                    "0 0 0 .2rem rgba(0, 123, 255, .25)"),
+                    (editor.getContainer().style.borderColor = "#80bdff");
+            });
+            editor.on("blur", () => {
+                (editor.getContainer().style.boxShadow = ""),
+                    (editor.getContainer().style.borderColor = "");
+            });
+        }
+    });
+    // tinymce-editor end
 
 })(jQuery);
